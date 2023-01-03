9c3683fa1e7f79146de40991b4a98dc2cace020f
daniscript18 <daniscript18@outlook.com>
2023-01-03 19:29:42 -0400

    Added graphical interface to the Python code The project was absolutely dedicated to Python and JavaScript will not be involved here.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Crypto Calculator
-This GitHub project is a calculator to help you calculate your cryptocurrency and dollar earnings on an hourly or daily timeline. It's made with JavaScript and HTML for the user interface, with additional code for Python hobbyists.
+Este proyecto de GitHub es una calculadora para ayudarlo a calcular sus ganancias en criptomonedas y en dólares (o cualquier otra moneda) en una línea de tiempo establecida por hora y/o por día. Originalmente se hizo con HTML y JavaScript pero ahora el proyecto se ha dedicado totalmente a Python.
 
-To use it you just need to open the `.html` file or run the `.py` file with Python, then answer the questions and it will instantly give you your results.
+Para usarlo, solo necesita descargar y abrir el archivo [`.exe`](https://github.com/daniscript18/crypto-calculator/releases) o ejecutar el archivo [`.py`](/calculator.py) con Python, luego llene los cuadros con la información necesario y dele a calcular.
 
-I will be improving the calculator and adding functions, if you think it would be good to add another function to the calculator or you find any problem, do not forget to notify me, also, maybe I could write the calculator in other languages.
+Estaré mejorando la calculadora y agregando funciones, si crees que sería bueno agregar otra función a la calculadora o encuentras algún problema, no olvides notificarme, también, tal vez podría escribir la calculadora en otros idiomas.
+
+# Images
+<img title="a title" alt="Alt text" src="https://i.ibb.co/9cNxrw9/bandicam-2023-01-03-19-21-00-337.jpg">
+<img title="a title" alt="Alt text" src="https://i.ibb.co/mC3VgBH/bandicam-2023-01-03-19-25-35-745.jpg">
--- calculator.py
+++ calculator.py
@@ -1,32 +1,56 @@
-def is_float(value):
-    try:
-        float(value)
-    except ValueError:
-        print("The value you entered is invalid")
-        exit()
+import tkinter
+import tkinter.messagebox
+import webbrowser
 
-def is_str(value):
-    try:
-        str(value)
-    except ValueError:
-        print("The value you entered is invalid")
-        exit()
+window = tkinter.Tk()
+window.title("Calculator")
+window.geometry("400x400")
 
-difficulty = input("Difficulty per block = ") or "none"; is_float(difficulty); difficulty = float(difficulty);
-hashrate = input("Miner hashrate = ") or "none"; is_float(hashrate); hashrate = float(hashrate)
-reward = input("Block reward = ") or "none"; is_float(reward); reward = float(reward)
-price = input("Coin price = ") or "none"; is_float(price); price = float(price)
+difficulty_value = tkinter.StringVar()
+hashrate_value = tkinter.StringVar()
+reward_value = tkinter.StringVar()
+price_value = tkinter.StringVar()
+cripto_value = tkinter.StringVar()
+currency_value = tkinter.StringVar()
 
-cripto = input("Crypto prefix (bitcoin) = ") or "bitcoin"; is_str(cripto); cripto = str(cripto)
-currency = input("Currency prefix (dollars) = ") or "dollars"; is_str(currency); currency = str(currency)
+difficulty_tag = tkinter.Label(window, text = "Difficulty per block"); difficulty_tag.grid(row = 0, column = 0)
+hashrate_tag = tkinter.Label(window, text = "Miner hashrate"); hashrate_tag.grid(row = 1, column = 0)
+reward_tag = tkinter.Label(window, text = "Block reward"); reward_tag.grid(row = 2, column = 0)
+price_tag = tkinter.Label(window, text = "Coin price"); price_tag.grid(row = 3, column = 0)
+crypto_tag = tkinter.Label(window, text = "Crypto prefix"); crypto_tag.grid(row = 4, column = 0)
+currency_tag = tkinter.Label(window, text = "Currency prefix"); currency_tag.grid(row = 5, column = 0)
 
-block_time = (difficulty / hashrate)
-coin_hour = ((3600 / block_time) * reward)
-coin_day = (coin_hour * 24)
-earnings_hour = (coin_hour * price)
-earnings_day = (coin_day * price)
+difficulty_field = tkinter.Entry(window, textvariable=difficulty_value); difficulty_field.grid(row = 0, column = 1)
+hashrate_field = tkinter.Entry(window, textvariable=hashrate_value); hashrate_field.grid(row = 1, column = 1)
+reward_field = tkinter.Entry(window, textvariable=reward_value); reward_field.grid(row = 2, column = 1)
+price_field = tkinter.Entry(window, textvariable=price_value); price_field.grid(row = 3, column = 1)
+crypto_field = tkinter.Entry(window, textvariable=cripto_value); crypto_field.grid(row = 4, column = 1); crypto_field.insert(0, "bitcoin")
+currency_field = tkinter.Entry(window, textvariable=currency_value); currency_field.grid(row = 5, column = 1); currency_field.insert(0, "dollars")
 
-print(f"» Earnings per hour in {cripto}: {coin_hour}")
-print(f"» Earnings per day in {cripto}: {coin_day}")
-print(f"» Earnings per hour in {currency}: {earnings_hour}")
-print(f"» Earnings per day in {currency}: {earnings_day}")
+def calculate(difficulty, hashrate, reward, price, cripto, currency):
+    block_time = (difficulty / hashrate)
+    coin_hour = ((3600 / block_time) * reward)
+    coin_day = (coin_hour * 24)
+    earnings_hour = (coin_hour * price)
+    earnings_day = (coin_day * price)
+
+    coin_hour_result = tkinter.Label(window, text = f"Earnings per hour in {cripto}: {coin_hour}"); coin_hour_result.grid(row = 7, column = 0)
+    coin_day_result = tkinter.Label(window, text = f"Earnings per day in {cripto}: {coin_day}"); coin_day_result.grid(row = 8, column = 0)
+    earnings_hour_result = tkinter.Label(window, text = f"Earnings per hour in {currency}: {earnings_hour}"); earnings_hour_result.grid(row = 9, column = 0)
+    earnings_day_result = tkinter.Label(window, text = f"Earnings per day in {currency}: {earnings_day}"); earnings_day_result.grid(row = 10, column = 0)
+
+def check_values():
+    if difficulty_value.get() == "" or hashrate_value.get() == "" or reward_value.get() == "" or price_value.get() == "" or cripto_value.get() == "" or currency_value.get() == "":
+        return tkinter.messagebox.showerror("Error", "No values ​​have been set in one or more fields.")
+    else:
+        try:
+            calculate(float(difficulty_value.get()), float(hashrate_value.get()), float(reward_value.get()), float(price_value.get()), str(cripto_value.get()), str(currency_value.get()))
+        except ValueError:
+            return tkinter.messagebox.showerror("Error", "One or more variables may have an invalid value.")
+
+button = tkinter.Button(window, text = "Calculate", command = check_values); button.grid(row = 6, column = 1)
+
+github = tkinter.Label(window, text = "Created by Dᥲᥒιᥱᥣ", fg="blue", cursor="hand2"); github.grid(row = 11, column = 0)
+github.bind("<Button-1>", lambda e: webbrowser.open_new("https://github.com/daniscript18"))
+
+window.mainloop()
